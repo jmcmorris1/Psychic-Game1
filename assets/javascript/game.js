ef7c14ca4319db7fdf25a2938eec92acc91873fb
Jameel McMorris <jmac54@belsouth.net>
2017-06-22 15:18:35 -0400

    I know what you're thinking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 var losses = 0;
@@ -12,30 +12,30 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 
 
 var updateGuessesLeft = function() {
-  document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
 
 
 
 var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
 
 
 var updateGuessesSoFar = function() {
-  document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
 
 var reset = function() {
-  totalGuesses = 9;
-  guessesLeft = 9;
-  guessedLetters = [];
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
 
-  updateLetterToGuess();
-  updateGuessesLeft();
-  updateGuessesSoFar();
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
 }
 
 updateLetterToGuess();
@@ -44,23 +44,21 @@ updateGuessesLeft();
 
 document.onkeyup = function(event) {
     guessesLeft--;
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-  guessedLetters.push(userGuess);
-  updateGuessesLeft();
-  updateGuessesSoFar();
+    guessedLetters.push(userGuess);
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
-        if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                reset();
-            }
-        }else if(guessesLeft == 0){ 
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+    if (guessesLeft > 0) {
+        if (userGuess == letterToGuess) {
+            wins++;
+            document.querySelector('#wins').innerHTML = "Wins: " + wins;
             reset();
         }
+    } else if (guessesLeft == 0) {
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        reset();
+    }
 };
-
-
